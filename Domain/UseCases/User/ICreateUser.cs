baac7a7feddfcbc9c11c7e82694029458a67aa4d
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 00:57:15 -0300

    feat: create usecase and interface createuser

---
+++ Domain/UseCases/User/ICreateUser.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.User.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UseCases.User
+{
+    public interface ICreateUser
+    {
+        Task CreateUser(UserModel userModel);
+    }
+}

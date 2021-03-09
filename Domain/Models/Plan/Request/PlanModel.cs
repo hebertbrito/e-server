78fca4bda92e8192a7f72961012a97a1e2881925
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 02:56:02 -0300

    feat: create domain features to createplanuser

---
+++ Domain/Models/Plan/Request/PlanModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models.Plan.Request
+{
+    public class PlanModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Plan { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string StartDatePlan { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string EndDatePlan { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string TypePerson { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string RegisterDate { get; set; }
+    }
+}
---
+++ Domain/UseCases/Plan/ICreatePlan.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.Plan.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UseCases.Plan
+{
+    public interface ICreatePlan
+    {
+        Task CreatePlanUser(PlanModel userModel);
+    }
+}

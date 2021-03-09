15b58e11f79679c5176c21bdfc95ae12f01e9413
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 01:05:55 -0300

    feat: create protocols createuserrepo

---
+++ Data/Protocols/Database/User/ICreateUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.User.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Protocols.Database.User
+{
+    public interface ICreateUserRepository
+    {
+        Task CreateUser(UserModel userModel);
+    }
+}

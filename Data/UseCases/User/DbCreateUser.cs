9a9ff74cebf02b7e21795dfbf1f4b029287d3fa2
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 01:09:26 -0300

    feat: create constructor on class DbCreateUser

--- Data/UseCases/User/DbCreateUser.cs
+++ Data/UseCases/User/DbCreateUser.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models.User.Request;
+﻿using Data.Protocols.Database.User;
+using Domain.Models.User.Request;
 using Domain.UseCases.User;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,15 @@ namespace Data.UseCases.User
 {
     public class DbCreateUser : ICreateUser
     {
+        private readonly ICreateUserRepository createUserRepository;
+        public DbCreateUser(ICreateUserRepository createUserRepository)
+        {
+            this.createUserRepository = createUserRepository;
+        }
+
         public async Task CreateUser(UserModel userModel)
         {
-            throw new NotImplementedException();
+            await this.createUserRepository.CreateUser(userModel);
         }
     }
 }

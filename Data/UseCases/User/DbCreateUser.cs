5c238860920b7025fabed9ac24df7288ba8a0dce
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 01:03:04 -0300

    feat: create data layer and usecase dbcreateuser

---
+++ Data/UseCases/User/DbCreateUser.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.User.Request;
+using Domain.UseCases.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.UseCases.User
+{
+    public class DbCreateUser : ICreateUser
+    {
+        public async Task CreateUser(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

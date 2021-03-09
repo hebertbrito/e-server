1da387164c9bdb2f396fa1c8fc99f2238fcac1d2
hebertbrito <hebertfelipe.97@outlook.com.br>
2021-03-09 00:53:21 -0300

    feat: create domain layer and user model

---
+++ Domain/Models/User/Request/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models.User.Request
+{
+    public class UserModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CPF { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string RG { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CNPJ { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Telephone { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string DateBirth { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string RegisterDate { get; set; }
+    }
+}
